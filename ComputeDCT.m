a688b4d53dba58fe8e2eff722b619ac679cfdcfb
sulaimanbehzad <sulaimanbehzad@gmail.com>
2021-06-07 23:41:12 +0430

    dct transform function added

--- ComputeDCT.m
+++ ComputeDCT.m
@@ -1,9 +1,42 @@
-function [block] = ComputeDCT(img)
+function [block] = ComputeDCT(img, A, B)
 %COMPUTEDCT Summary of this function goes here
 %   Detailed explanation goes here
-T = dctmtx(8);
-dct =@(block_struct) T * block_struct.data * T';
-block = blockproc(img, [8 8], dct);
-% block = inputArg1;
+[imY, imX] = size(img);
+im_transform.block = zeros(A,B);
+normalization_matrix=[
+    16 11 10 16 24 40 51 61
+    12 12 14 19 26 58 60 55
+    14 13 16 24 40 57 69 56
+    14 17 22 29 51 87 80 62
+    18 22 37 56 68 109 103 77
+    24 35 55 64 81 104 113 92
+    49 64 78 87 103 121 120 101
+    72 92 95 98 112 100 103 99];
+for a=1:imY/A
+    for b=1:imX/B
+        for k=1:A
+            for l=1:B
+                res=0;
+                for i=1:A
+                    for j=1:B
+                        res=res+double(img(A*(a-1)+i,B*(b-1)+j))*cos(pi*(k-1)*(2*i-1)/(2*A))*cos(pi*(l-1)*(2*j-1)/(2*B));    
+                    end
+                     if k==1
+                         res = res*sqrt(1/A);
+                     else
+                         res = res*sqrt(2/A);
+                     end
+                     if l==1
+                         res = res*sqrt(1/B);
+                     else
+                         res = res*sqrt(2/B);
+                     end
+                     im_transform(a,b).block(k,l)=res;
+                end
+            end
+        end
+        im_transform(a,b).block = (im_transform(a,b).block./normalization_matrix);
+        im_transform.block
+    end
 end
-
+block = im_transform.block;
--- main.m
+++ main.m
@@ -1,17 +1,20 @@
-clear all;
+clear
 close all;
 clc;
 
 imageFiles = dir('grayscale/*.png');
 nImages = length(imageFiles);
 imageDir = imageFiles.folder;
-for i=1:nImages
+for i=1:nImages-48
     curImageName = [imageDir '\' imageFiles(i).name];
 %     printf(string(curImageName));
     curImage = imread(curImageName);
 %     change image to do
     curImageDouble = im2double(curImage);
-    dct2(
-%     dct{i}=ComputeDCT(curImageDouble);
+%     dct2(
+% Dimensions for DCT blocksize
+    A=8;
+    B=8;
+    dct{i}=ComputeDCT(curImageDouble, A, B);
     images{i}=curImage;
 end
