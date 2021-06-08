3b6e3afd278d0e1325f4f764c033d2326351c804
sulaimanbehzad <sulaimanbehzad@gmail.com>
2021-06-08 15:47:17 +0430

    fixed

--- endcoder.m
+++ endcoder.m
@@ -11,12 +11,12 @@ for i=1:nImages-48
     curImage = imread(curImageName);
     curImage = imresize(curImage, [512, 512]);
 %     change image to do
-    curImageDouble = im2double(curImage);
+%     curImageDouble = im2double(curImage);
 % Dimensions for DCT blocksize
     A=8;
     B=8;
-    [imX, imY] = size(curImageDouble);
-    curDct=ComputeDCT(curImageDouble, A, B, imX, imY);
+    [imX, imY] = size(curImage);
+    curDct=ComputeDCT(curImage, A, B, imX, imY);
     curZigZag = ComputeZigZag(curDct, A, B, imX, imY, 8, 8);
     curRunLength = RunLength(curZigZag, A, B, imX, imY);
     save(['runlength/' imageFiles(i).name '.mat'], 'curRunLength');
