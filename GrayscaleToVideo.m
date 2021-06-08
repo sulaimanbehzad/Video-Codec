bed9be31ed549f4b251f90c8b5737b286a268642
sulaimanbehzad <sulaimanbehzad@gmail.com>
2021-06-08 18:08:27 +0430

    final edit is done! YAYYYY!

--- GrayscaleToVideo.m
+++ GrayscaleToVideo.m
@@ -17,8 +17,12 @@ for i=1:nFiles
     images{i} = imread(curFileName);
 end
 for k=1:length(images)
-    rgbImage = ind2rgb(images{k}, jet(256));
-    frame = im2frame(rgbImage);
+    rgbImage = zeros(512, 512, 3);
+    rgbImage(:,:,1)=images{k};
+    rgbImage(:,:,2)=images{k};
+    rgbImage(:,:,3)=images{k};
+%     rgbImage = ind2rgb(images{k}, jet(256));
+    frame = im2frame(uint8(rgbImage));
     writeVideo(vidWriter,frame);
 end
 close(vidWriter);
